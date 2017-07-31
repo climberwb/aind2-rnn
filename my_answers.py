b56e41ce21b1e5351ba99f334b10e3db57782695
climberw <aind2@ip-172-31-8-202.ec2.internal>
2017-07-31 00:20:43 +0000

    completed regex filter started window function for text

--- my_answers.py
+++ my_answers.py
@@ -3,7 +3,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-
+import re
 
 
 # TODO: fill out the function below that transforms the input series 
@@ -35,8 +35,11 @@ def build_part1_RNN(window_size):
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
+  
     punctuation = ['!', ',', '.', ':', ';', '?']
-
+    text = text.lower()
+    
+    text = re.sub(r"[^a-z0-9%s]"%(''.join(punctuation)), " ", text)
     return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -44,7 +47,17 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
-
+    series = text.split()
+    step=0
+    for i in range(len(series[:-window_size])):
+        step = step_size+i
+        inputs.append(series[step:step+window_size])
+        if step >= len(series[:-window_size]):
+            break
+    #print(inputs)
+    #outputs = np.asarray(outputs)
+    #inputs = np.asarray(inputs)
+    print(outputs)
     return inputs,outputs
 
 # TODO build the required RNN model: 
