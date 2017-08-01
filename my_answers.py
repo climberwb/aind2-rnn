6c5178f0b4592c93bff66956bbe65690fed9eb20
climberw <aind2@ip-172-31-8-202.ec2.internal>
2017-08-01 18:59:48 +0000

    added 200 hidden units to lstm

--- my_answers.py
+++ my_answers.py
@@ -62,6 +62,6 @@ def window_transform_text(text, window_size, step_size):
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
     model = Sequential()
-    model.add(LSTM(5, input_shape=(window_size, num_chars)))
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
     model.add(Dense(num_chars,activation='softmax'))
     return model
